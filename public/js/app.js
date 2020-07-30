0553b987b667c31dda1cb8789ef6b53cc3720833
nadeem-mmd <nadim414@gmail.com>
2020-07-31 03:08:34 +0530

    removed commented code

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-//console.log('rendering client side javascript');
-
-// fetch('http://localhost:3000/weather?address=hyderabad')
-//     .then(response => {
-//         response.json().then(data => {
-//             if (data.error)
-//                 console.log(error);
-//             else {
-//                 console.log(data)
-//             }
-//         });
-//     });
-
 const weatherForm = document.querySelector('form');
 const weatherInput = document.querySelector('input');
 const msg1 = document.querySelector('#message-1');
